708b38ff22e0a251f99b9881c3900fd61c8dee17
carroteasy <120318940+carroteasy@users.noreply.github.com>
2024-03-17 17:42:14 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  for(char i='a'; i<='z'; i++) {
+    cout << (int)s.find(i) << ' ';
+  }
+}
